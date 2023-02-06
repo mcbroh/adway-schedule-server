edd9879c1d54c34546a31d48d6e0123d8bcf5b5f
Jamiu Makanjuola <mcbroh@msn.com>
2023-02-06 21:03:54 +0100

    copy folder over on build

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cors from "@fastify/cors";
 import Fastify from "fastify";
 
 import { initRoutes } from "./router/schemaRoutes";
+import getSchedule from "./utils/getSchedule";
 
 export const server = Fastify();
 
@@ -14,6 +15,9 @@ async function main() {
     const port = process.env.PORT || 8080;
     await server.listen({ port: 8080 });
 
+    const schedule = await getSchedule();
+    console.log(schedule);
+    
     console.log(`Server ready on port ${port}`);
   } catch (error) {
     console.log(error);

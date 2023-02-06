a2b24d2fc95b5b51835bf6e9d294ab278868382a
Jamiu Makanjuola <mcbroh@msn.com>
2023-02-06 20:16:19 +0100

    made prot dynamic in prod

--- src/app.ts
+++ src/app.ts
@@ -8,12 +8,13 @@ export const server = Fastify();
 async function main() {
   // routes must be initiated last
   await initRoutes(server);
-  await server.register(cors)
+  await server.register(cors);
 
   try {
+    const port = process.env.PORT || 8080;
     await server.listen({ port: 8080 });
 
-    console.log(`Server ready on port http://localhost:8080`);
+    console.log(`Server ready on port ${port}`);
   } catch (error) {
     console.log(error);
     process.exit(1);

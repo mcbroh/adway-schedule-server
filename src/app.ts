2ebb12ed95cb66c871b91053bb6e4041750d312d
Jamiu Makanjuola <mcbroh@msn.com>
2023-02-07 09:43:05 +0100

    Added main entry point

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,20 @@ import { initRoutes } from "./router/schemaRoutes";
 
 export const server = Fastify();
 
-server.register(cors);
-// routes must be initiated last
-initRoutes(server);
+async function main() {
+  await server.register(cors);
+  // routes must be initiated last
+  await initRoutes(server);
 
-try {
-  const port = process.env.PORT || 8080;
-  server.listen({ port: 8080 });
+  try {
+    const port = process.env.PORT || 8080;
+    await server.listen({ port: 8080 });
 
-  console.log(`Server ready on port ${port}`);
-} catch (error) {
-  console.log(error);
-  process.exit(1);
+    console.log(`Server ready on port ${port}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 }
+
+main();

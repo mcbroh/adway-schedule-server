b94e54e68ffe40870ef11a96fc00b010e06f0d7f
Jamiu Makanjuola <mcbroh@msn.com>
2023-02-07 13:39:25 +0100

    trying fast direct

--- src/server.ts
+++ src/server.ts
@@ -5,21 +5,28 @@ import Fastify from "fastify";
 import { initRoutes } from "./router/schemaRoutes";
 
 export const server = Fastify();
-import express from "express";
+/* import express from "express";
+
+
 const app = express();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(HTTP_PORT);
+app.listen(HTTP_PORT); */
 
-async function main() {
-  await server.register(cors);
+server.get('/', async (request, reply) => {
+  return { hello: 'world' }
+})
+
+
+const start = async () => {
+  // await server.register(cors);
   // routes must be initiated last
-  await initRoutes(server);
+  // await initRoutes(server);
 
   try {
-    // await server.listen({ port: 8080 });
+    await server.listen({ port: 8080 });
 
     console.log(`Server ready on port ${HTTP_PORT}`);
   } catch (error) {
@@ -28,4 +35,4 @@ async function main() {
   }
 }
 
-main();
+start();
